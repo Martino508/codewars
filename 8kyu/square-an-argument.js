1be90d5616d3950b8543bc2d3bb60ff206397782
Martin <martinpaulcarpio@gmail.com>
2023-12-01 19:38:02 -0500

    square an argument code

---
+++ 8kyu/square-an-argument.js
@@ -0,0 +1,10 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// My Solution
+
+function square(num){
+    return num ** 2;
+}
+
+//Best Practice
+const square = (n) => n * n;
